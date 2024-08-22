1d4a6f48d81e30be4dfdc059bba47fb13de4b01f
Carlos Arriagada <carl.arriagadacl@gmail.com>
2024-08-22 13:50:50 -0400

    added navbar and home sections

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Home() {
-  return (
-    <section className="text-3x1 flex min-h-screen justify-center items-center">
-      <h2 className="font-semibold">App Medica</h2>
-    </section>
-  );
-}
