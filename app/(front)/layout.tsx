4233fb39b649812270f0066cd2726eb6613797a6
Carlos Arriagada <carl.arriagadacl@gmail.com>
2024-08-28 03:07:32 -0400

    remove garbage from mongo

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -6,10 +6,10 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="bg-white">
       <Navbar />
-      <div className="bg-white mx-auto py-4 fixed top-20 w-full left-0 z-50 right-0 border-t border-gray-400/30 container">
+      {/* <div className="bg-white mx-auto py-4 fixed top-20 w-full left-0 z-50 right-0 border-t border-gray-400/30 container">
         <MegaMenu />
-      </div>
-      <div className="mt-[80px]">{children}</div>
+      </div> */}
+      <div className="mt-[60px]">{children}</div>
     </div>
   );
 }
